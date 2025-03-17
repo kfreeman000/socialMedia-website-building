777a76b199547abc4a32d05840dd31b238114f8a
Katherine Freeman <157547841+kfreeman000@users.noreply.github.com>
2025-03-17 14:44:18 -0400

    Add files via upload
    npx structure created and uploaded

---
+++ app/socialMedia-website-building/app.js
@@ -0,0 +1,17 @@
+import {
+    Image,
+    Text,
+    TouchableOpacity,
+    View
+  } from 'react-native';    //  react native components you must import to manipulate variables 
+
+
+
+
+
+
+  function HomeScreen({navigation}) {
+    return (
+      <ProfileForm />
+    );
+  }
